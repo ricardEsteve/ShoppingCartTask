0be5600295eb4e9568e3ff3f72f500d5e61256c5
Ricard <ricard.esteve2405@outook.com>
2017-07-12 10:31:14 +0200

    ShoppingCart Task modified for testing
    Only one line removed. (first steps...)

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ public class Main {
 
     public static void main(String [ ] args)
     {
-
         Product p1 = new Product ("vacuum cleaner", 100);
         Product p2 = new Product("pencil", 3);
         Product p3 = new Product("bike", 300);
